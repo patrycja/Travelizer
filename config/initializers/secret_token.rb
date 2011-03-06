668303da126c618ad2b5bd4db7fcde9fd909bab0
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-03-06 22:21:07 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelizer::Application.config.secret_token = '1bb3df7a64f25a178a6a3446c9402f26e4dda4ed19b1627818de9166b6e2707f87aa2bde037ac906defd3eb98f67ef1000bd2e51f5f7997895ea8960fa162e4d'
