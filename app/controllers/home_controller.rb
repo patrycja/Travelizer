b64083776d9b216e04949d5fd552fae48492fce3
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-03-06 23:41:46 +0100

    Dodane nicki użytkowników, profile publiczne(/nazwa_uzytkownika), strony użytkownika zalogowanego(pod /)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
+# controller for home index for creating index and profile sites 
+
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      render "profile"
+    else
+      render "index"
+    end
   end
 
+  def public_profile
+    @username = params[:username] 
+    @user = User.where("username = ?", @username).first
+    if @user 
+    else
+      render "username_not_found"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Travelizer::Application.routes.draw do
   get "home/index"
   devise_for :users
 
+  root :to => "home#index"
+  match "/:username/" => "home#public_profile", :as=>"public_url"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,7 +55,7 @@ Travelizer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  # root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
